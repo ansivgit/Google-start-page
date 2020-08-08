16712179ccd2e526d863d17d76d4bf02d9bd788a
AnnaZAS <ansiv@yandex.ru>
2020-08-08 20:11:02 +0300

    finished task

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const menuSettings = document.querySelector('.menu-settings');
+const menuOpen = ((event) => {
+  const menuSettings = document.querySelector('.menu-settings');
+  const menuApplications = document.querySelector('.menu-applications');
 
-const menuSettingsOpen = ((event) => {
   const settingsBlock = event.target.closest('.footer-content__menu-item--settings');
+  const applicationsBtn = event.target.closest('.main-nav__content--applications');
 
   settingsBlock ? menuSettings.classList.toggle('hide') : menuSettings.classList.add('hide');
+  applicationsBtn ? menuApplications.classList.toggle('hide') : menuApplications.classList.add('hide');
 });
 
-document.addEventListener('click', menuSettingsOpen);
+document.addEventListener('click', menuOpen);
